9ef253e881241a8797a649ffe12e83bcaaeb7b76
verja <janneth_verduzco@live.com>
2020-11-25 23:26:34 -0600

    added question and dogbreed objtect

--- script.js
+++ script.js
@@ -1,30 +1,122 @@
 $(document).ready(function () {
+    // alert("hello world")
 
     // VARIABLES
-    // =====================================================================
-    
-    // var apiKey = "&a5ce4bb6-b633-4cd7-a50e-014671a60a07";
-
+    // =====================================================================\\
+    // quiz question count
+    var questionCount = 0;
+    // quiz questions
+    var questions = [
+        {
+            question1: "What size dog are you looking for?",
+            choice1: "small",
+            choice2: "medium",
+            choice3: "large",
+        },
+        {
+            question2: "What kind of temperment are you looking for?",
+            choice1: "Friendly",
+            choice2: "Obedient",
+            choice3: "Independent",
+            choice4: "Fearless"
+        },
+        {
+            question2: "What kind of activity are you looking for?",
+            choice1: "Herding",
+            choice2: "Guarding",
+            choice3: "Working",
+            choice4: "Sporting"
+        }
+    ]
+    // options of dog breeds and information
+    var dogBreed = [
+        {
+            name: "Husky",
+            size: "Large",
+            temperment: "Friendly",
+            breedGroup: "Working"
+        },
+        {
+            name: "German Shepard",
+            size: "Large",
+            temperment: "Courageous",
+            breedGroup: "Herding"
+        },
+        {
+            name: "Saint Bernard",
+            size: "Small",
+            temperment: "Friendly",
+            breedGroup: "Working"
+        },
+        {
+            name: "Beagle",
+            size: "Small",
+            temperment: "Friendly",
+            breedGroup: "Hound"
+        },
+        {
+            name: "Doberman",
+            size: "Large",
+            temperment: "Courageous",
+            breedGroup: "Working",
+        },
+        {
+            name: "Belgian Malinois",
+            size: "Large",
+            temperment: "Friendly",
+            breedGroup: "Herding",
+        },
+        {
+            name: "Greyhound",
+            size: "Medium",
+            temperment: "Intelligent",
+            breedGroup: "Hound",
+        },
+        {
+            name: "Border Collie",
+            size: "Medium",
+            temperment: "Intelligent",
+            breedGroup: "Herding"
+        },
+        {
+            name: "Boxer",
+            size: "Medium",
+            temperment: "Courageous",
+            breedGroup: "Working"
+        },
+        {
+            name: "Poodle",
+            size: "Large",
+            temperment: "Intelligent",
+            breedGroup: "Working"
+        },
+    ]
 
     // LISTENERS
     // =====================================================================
 
-
-
     // FUNCTIONS
     // =====================================================================
-    function searchBreed(breed) {
-        var queryUrl = "https://api.thedogapi.com/v1/" + breed;
-        
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (breedResults) {
-        var height = breedResults.height.imperial;
-        console.log(breedResults)
-    })
 
+    // questions for loop
+    for (var i = 0; i < questions.length; i++) {
+        var currentQuestion = questions[i];
+        console.log(questions)
+
+        if (currentQuestion[0]) {
+        }
     }
+
+     // ajax call to fetch breed image to dogCEO api
+    var  breeds = ["germanshepherd", "husky", "stbernard", "beagle", "doberman", "malinois", "greyhound", "collie", "boxer", "poodle"]
+    var selectedBreed = "collie"
+    var queryURL = "https://dog.ceo/api/breed/" + selectedBreed + "/images"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (dogRes) {
+        console.log(dogRes.message[0])
+    });
 
 
 });
